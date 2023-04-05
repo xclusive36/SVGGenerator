016a1a0c64536d0a48a5c4da7f06557ae301c914
Joshua Cavell <xclusive36@gmail.com>
2023-04-05 18:16:46 -0400

    switched back to classes for shapes per instructions

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
-// AS a freelance web developer
-// I WANT to generate a simple logo for my projects
-// SO THAT I don't have to pay a graphic designer
-
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for text
-// THEN I can enter up to three characters
-// WHEN I am prompted for the text color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-// WHEN I am prompted for a shape
-// THEN I am presented with a list of shapes to choose from: circle, triangle, and square
-// WHEN I am prompted for the shape's color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-// WHEN I have entered input for all the prompts
-// THEN an SVG file is created named `logo.svg`
-// AND the output text "Generated logo.svg" is printed in the command line
-// WHEN I open the `logo.svg` file in a browser
-// THEN I am shown a 300x200 pixel image that matches the criteria I entered
-
-// must utilize OOP and SOLID principles
-// must use at least one class
-// must use at least one method
-// must use at least one constructor
-
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
-const Shape = require("./lib/shape");
 const writeFileAsync = util.promisify(fs.writeFile);
 const validateColor = require("validate-color").default;
+const Square = require("./lib/square");
+const Triangle = require("./lib/triangle");
+const Circle = require("./lib/circle");
 
 console.log();
 
@@ -83,8 +61,16 @@ const promptUser = () => {
 const init = () => {
   promptUser()
     .then((answers) => {
-      console.log(answers);
-      const shape = new Shape(answers);
+      let shape;
+      if (answers.shape === "square") {
+        shape = new Square(answers);
+      } else if (answers.shape === "triangle") {
+        shape = new Triangle(answers);
+      } else if (answers.shape === "circle") {
+        shape = new Circle(answers);
+      } else {
+        console.log("Error: Shape not found");
+      }
       return shape.generateShape();
     })
     .then((result) => {
@@ -96,5 +82,3 @@ const init = () => {
 };
 
 init();
-
-// https://www.npmjs.com/package/validate-color
